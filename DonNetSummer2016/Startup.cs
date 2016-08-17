08e57fc7c9cf087ba579fb68f347f6874c5fbecf
proxy51 <rpaabor@gmail.com>
2016-08-17 10:38:39 +0200

    JA en ny jävla version finns här nu då Svenne! Vart e mina pengar?

---
+++ DonNetSummer2016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DonNetSummer2016.Startup))]
+namespace DonNetSummer2016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
